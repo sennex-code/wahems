67270f0c8962c62ab3a5289c594e1d0e649cfde2
sennex <laincaputol21@gmail.com>
2026-09-23 16:58:13 +0800

    sadasdas

--- config/services.php
+++ config/services.php
@@ -9,8 +9,8 @@ return [
     |
     | This file is for storing the credentials for third party services such
     | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
+    | location for this type of information, allowing packages to have a
+    | conventional file to locate the various service credentials.
     |
     */
 
@@ -21,9 +21,7 @@ return [
     'resend' => [
         'key' => env('RESEND_API_KEY'),
     ],
-    'google' => [
-        'key_json' => json_decode(env('GOOGLE_CLOUD_KEY_JSON'), true)
-    ],
+
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
